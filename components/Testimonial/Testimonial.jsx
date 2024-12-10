d00dd90089b9054bb72b31bf498fa2fc5cc3d373
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-12-10 20:20:25 +1000

    Finished Testimonial component
    Just need to do documentation

--- components/Testimonial/Testimonial.jsx
+++ components/Testimonial/Testimonial.jsx
@@ -1,5 +1,27 @@
+import classnames from 'classnames'
+import { BiSolidQuoteLeft } from "react-icons/bi";
 
 
-export default function Testimonial() {
 
+import testImg from './testImage/testImage.jfif'
+
+export default function Testimonial({imgSrc,
+     imgAlt='',
+     personName='May Andersons',
+     personOccupation='Workcation, CTO',
+     children}) {
+        const classes = classnames('testimonial', 'testimonial-with-img')
+        return (
+            <div className={classes}>
+                {imgSrc && <img src={imgSrc} alt={imgAlt} />}
+                <div className='testimonial-text'>
+                    <BiSolidQuoteLeft color='#5C74CA' />
+                    <p>{children}</p>
+                    <div className='testimonial-footer'>
+                        <p className='testimonial-name'>{personName}</p>
+                        <p>{personOccupation}</p>
+                    </div>
+                </div>
+            </div>
+        )
 }
